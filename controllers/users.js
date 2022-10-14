16be09b942517c0eeb3fd76cedd9d36143090896
Robert <lactoz22@gmail.com>
2022-10-14 18:51:31 -0500

    Created the users router object and exported to the app.js. This file will process the post requests and create new users

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res) => {
+  const { username, name, password } = req.password;
+
+  if (!username || username.length < 3 || !password || password.length < 4 || !name) return res.status(400).end();
+
+  const repeatedUser = await User.findOne({ username });
+  if (repeatedUser) return res.status(400).json({error: 'Username is already taken'});
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  });
+
+  const savedUser = await user.save();
+
+  res.status(201).json(savedUser);
+});
+
+if (process.env.NODE_ENV === 'test') {
+  usersRouter.get('/', async (req, res) => {
+    const users = await User.find({});
+
+    res.status(200).json(users);
+  });
+}
+
+export default usersRouter;
