f95268ade06ea527a9ecf9e699a6ac429dbc5e6e
Robert <lactoz22@gmail.com>
2022-10-26 01:44:33 -0500

    Added a semicolon at the export line

--- utils/config.js
+++ utils/config.js
@@ -7,10 +7,4 @@ const DB_URI = (process.env.NODE_ENV === 'test')
   ? process.env.TEST_DB_URI
   : process.env.DB_URI;
 
-const rootUser = {
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-}
-
-
-export { PORT, DB_URI, rootUser };
+export { PORT, DB_URI };
